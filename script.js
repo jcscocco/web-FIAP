ce037b620e5931208b88ffd6383516d6b913dad8
Julio cesar da silva cocco <juliococco2801@gmail.com>
2026-03-17 20:45:20 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,88 @@
+
+    // console.log("Mensagem normal")
+
+    // console.info("Informação")
+
+    // console.warn("Aviso amrelo no site ")
+
+    // console.error("Erro vermelho no site")
+
+//-----------------------------------------
+
+// console.table([
+//     {
+//         id: 1, tarefa: "Estidar JS"
+//     },
+//     {
+//         id: 2, tarefa: "praticar codigo"
+//     }
+// ])
+
+// console.group("Grupo de logs ")
+// console.log('log 1')
+// console.log('log 2')
+// console.log('log 3')
+// console.groupEnd()
+
+// console.time("Timer ")
+
+// let casa = ""
+// if (casa == "casa"){
+//     console.log("casa")
+// }
+
+// console.timeEnd("Timer")
+
+// let UserNamer = "Maria"
+
+// const API_URL = "URL"
+
+// let texto1 = "ola"
+// console.log("nome:"+ " " + typeof (texto)+ " " + texto)
+
+// let numero  = 10
+// console.log("numero:"+ " " + typeof (numero)+ " " + numero)
+
+// let isComplete  = false
+// console.log("isComplete:"+ " " + typeof (isComplete)+ " " + isComplete)
+
+// let semValor
+// console.log("semValor:"+ " " + typeof (semValor)+ " " + semValor)
+
+// let nulo = null 
+// console.log("nulo" + "" + "tipo" + typeof (nulo))
+
+// let uniqueId = Symbol("ID")
+// console.log(typeof(uniqueId))
+// console.log(uniqueId)
+
+// let bigNumero = 9999999999n
+// console.log("bigNumero" + bigNumero + "tipo" +typeof(bigNumero))
+
+const alunos = [
+  { nome: "Julio", nota: 8 },
+  { nome: "Maria", nota: 7.5 },
+  { nome: "Pedro", nota: 6 },
+  { nome: "Ana", nota: 9 }
+];
+
+let soma = 0;
+
+console.log("Início do cálculo da média...\n");
+
+for (let i = 0; i < alunos.length; i++) {
+ 
+  console.log(`Aluno: ${alunos[i].nome}`);
+  console.log(`Nota: ${alunos[i].nota}`);
+  
+  soma += alunos[i].nota;
+  
+  console.log(`Soma parcial: ${soma}\n`);
+}
+
+const media = soma / alunos.length;
+
+console.log("Cálculo final:");
+console.log(`Total da soma: ${soma}`);
+console.log(`Quantidade de alunos: ${alunos.length}`);
+console.log(`Média da turma: ${media}`);
