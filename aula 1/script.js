ccb651752943886c95713c54b91053659f715712
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 20:45:31 -0300

    segunda aula

--- aula 1/script.js
+++ aula 1/script.js
@@ -59,30 +59,51 @@
 // let bigNumero = 9999999999n
 // console.log("bigNumero" + bigNumero + "tipo" +typeof(bigNumero))
 
-const alunos = [
-  { nome: "Julio", nota: 8 },
-  { nome: "Maria", nota: 7.5 },
-  { nome: "Pedro", nota: 6 },
-  { nome: "Ana", nota: 9 }
-];
+// const alunos = [
+//   { nome: "Julio", nota: 8 },
+//   { nome: "Maria", nota: 7.5 },
+//   { nome: "Pedro", nota: 6 },
+//   { nome: "Ana", nota: 9 }
+// ];
 
-let soma = 0;
+// let soma = 0;
 
-console.log("Início do cálculo da média...\n");
+// console.log("Início do cálculo da média...\n");
 
-for (let i = 0; i < alunos.length; i++) {
+// for (let i = 0; i < alunos.length; i++) {
  
-  console.log(`Aluno: ${alunos[i].nome}`);
-  console.log(`Nota: ${alunos[i].nota}`);
+//   console.log(`Aluno: ${alunos[i].nome}`);
+//   console.log(`Nota: ${alunos[i].nota}`);
   
-  soma += alunos[i].nota;
+//   soma += alunos[i].nota;
   
-  console.log(`Soma parcial: ${soma}\n`);
-}
+//   console.log(`Soma parcial: ${soma}\n`);
+// }
 
-const media = soma / alunos.length;
+// const media = soma / alunos.length;
 
-console.log("Cálculo final:");
-console.log(`Total da soma: ${soma}`);
-console.log(`Quantidade de alunos: ${alunos.length}`);
-console.log(`Média da turma: ${media}`);
+// console.log("Cálculo final:");
+// console.log(`Total da soma: ${soma}`);
+// console.log(`Quantidade de alunos: ${alunos.length}`);
+// console.log(`Média da turma: ${media}`);
+
+// let idade = Number(prompt("Digite sua idade:"))
+
+// let status = idade >= 18 ? "Maior de idade" : "Menor de idade"
+
+// console.log(idade)
+// console.log(status)
+
+// let valor = null 
+
+// let resultado = valor ?? "valor padrao"
+
+// Console.log(resultado)
+
+// let idade = Number(prompt("Quantos anos você tem?"))
+// let carteira = prompt("Você tem carteira? (sim/nao)")
+
+// if (idade >= 18 && carteira.toLowerCase() === "sim") {
+//   console.log("Pode dirigir")
+// } else {
+//   console.log("Não pode dirigir")
